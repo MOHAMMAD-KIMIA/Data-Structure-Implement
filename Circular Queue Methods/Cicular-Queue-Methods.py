b79da5cd02c9292ec101ea241037ce11a9115a2b
MOHAMMAD-KIMIA <129676631+MOHAMMAD-KIMIA@users.noreply.github.com>
2023-11-15 10:23:21 +0330

    Circular Queue Methods Implement

---
+++ Circular Queue Methods/Cicular-Queue-Methods.py
@@ -0,0 +1,52 @@
+class CircularQueue:
+
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None for i in range(size)] 
+        self.front = self.rear = -1
+
+    def isempty(self):
+        if self.front == -1:
+            return True
+        
+    def isfull(self):
+        if ((self.front == 0 and self.rear == -1) or ((self.rear+1) % self.size == self.front )):
+            return True
+
+    def peekQueue(self):
+        return self.queue[self.front]
+
+    def enQueue(self, val):
+        if self.isfull():
+            return False
+        
+        elif (self.front == -1):
+            self.front = self.rear = 0
+            self.queue[self.rear] = val
+
+        else:
+            self.rear = (self.rear + 1) % self.size 
+            self.queue[self.rear] = val
+
+    
+    def deQueue(self):
+        if self.isempty():
+            return False
+                    
+        elif (self.front == self.rear):
+            temp = self.queue[self.front]
+            self.front = self.rear = -1
+            return temp
+        
+        else:
+           temp = self.queue[self.front]
+           self.front = (self.front + 1) % self.size
+   
+    def reverseList(self): 
+        start = self.front
+        end = self.rear
+        A = self.queue
+        while start < end: 
+            A[start], A[end] = A[end], A[start] 
+            start += 1
+            end -= 1
