f38febf4336c4ef52652783c33e8fde443a1e4cd
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-21 19:50:25 +0330

    commenting Queue_Linkedlist

--- Linked List/Queue_Linkedlist.py
+++ Linked List/Queue_Linkedlist.py
@@ -1,19 +1,27 @@
+# Import the linkedList module from the One_Direction_Linkedlist package
 from One_Direction_Linkedlist import linkedList
 
+# Queue class represents a queue using a linked list
 class Queue:
+    # Initialize a linked list for the queue
     queue = linkedList.LinkedList()
     
+    # Method to enqueue (add) data to the end of the queue
     def enqueue(self, data):
-        self.queue.insertAtend(data)
+        self.queue.insertAtEnd(data)
             
+    # Method to dequeue (remove) data from the front of the queue
     def dequeue(self):
-        return self.queue.deleteFromlast()
+        return self.queue.deleteFromFirst()
         
+    # Method to peek at the data at the front of the queue without removing it
     def peek(self):
         return self.queue.head.data
     
+    # Method to get the size (number of elements) of the queue
     def size(self):
         return self.queue.sizeOf()
             
+    # Method to display the elements of the queue
     def display(self):
         self.queue.display()
