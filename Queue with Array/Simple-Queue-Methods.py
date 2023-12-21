dfb2f213c6e8c697c2c7131ea76a2385121d625f
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-21 11:22:24 +0330

    comment on Queue

--- Queue with Array/Simple-Queue-Methods.py
+++ Queue with Array/Simple-Queue-Methods.py
@@ -1,44 +1,46 @@
+# Queue class represents a simple queue data structure
 class Queue:
-    
     def __init__(self, size):
-        self.size = size 
-        self.queue = [None for i in range(size)]
-        self.front = self.rear = 0
-        
-        
-    def isempty(self):
-        if self.front == self.rear:
-            return True
-        
-    def isfull(self):
-        if (self.rear == self.size):
-            return True
-        
-    def peekQueue(self):
-        return self.queue[self.front]
-    
-    def enQueue(self, val):
-        if self.isfull():
-            return False
+        self.size = size  # Maximum capacity of the queue
+        self.queue = [None for i in range(size)]  # List to store queue elements
+        self.front = self.rear = 0  # Pointers to the front and rear of the queue
 
+    def isempty(self):
+        # Check if the queue is empty
+        return self.front == self.rear
+
+    def isfull(self):
+        # Check if the queue is full
+        return self.rear == self.size
+
+    def peekQueue(self):
+        # Return the front element without removing it
+        return self.queue[self.front]
+
+    def enQueue(self, val):
+        # Enqueue an element into the queue
+        if self.isfull():
+            return False  # Queue is full, cannot enqueue
         else:
             self.queue[self.rear] = val
-            self.rear = self.rear + 1
+            self.rear += 1
+            return True
 
     def deQueue(self):
+        # Dequeue an element from the queue
         if self.isempty():
-            return False
-        
+            return False  # Queue is empty, cannot dequeue
         else:
-            for i in range (rear-1):
-                self.queue[i] = self.queue[i+1]
-            rear = rear - 1
-        
+            for i in range(self.rear - 1):
+                self.queue[i] = self.queue[i + 1]
+            self.rear -= 1
+
     def reverseQueue(self):
+        # Reverse the elements in the queue
         start = self.front
         end = self.rear - 1
         A = self.queue
-        while start < end: 
-            A[start], A[end] = A[end], A[start] 
+        while start < end:
+            A[start], A[end] = A[end], A[start]
             start += 1
             end -= 1
