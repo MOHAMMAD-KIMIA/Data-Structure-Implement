7236c196e384a07a3adce35a8e376880e124a137
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-21 19:51:03 +0330

    commenting Stack_Linkedlist

--- Linked List/Stack_Linkedlist.py
+++ Linked List/Stack_Linkedlist.py
@@ -1,16 +1,23 @@
+# Import the linkedList module from the One_Direction_Linkedlist package
 from One_Direction_Linkedlist import linkedList
 
+# Stack class represents a stack using a linked list
 class Stack:
+    # Initialize a linked list for the stack
     stack = linkedList.LinkedList()
     
+    # Method to push (add) data to the top of the stack
     def push(self, data):
-        self.stack.insertAtend(data)
+        self.stack.insertAtEnd(data)
             
+    # Method to pop (remove) data from the top of the stack
     def pop(self):
-        return self.stack.deleteFromlast()
+        return self.stack.deleteFromLast()
     
+    # Method to peek at the data at the top of the stack without removing it
     def peek(self):
         return self.stack.head.data
             
+    # Method to display the elements of the stack
     def display(self):
         self.stack.display()
