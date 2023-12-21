3dfa6459305a406a040d28aa15b0bb782cf402ac
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-21 11:20:12 +0330

    comment stack

--- Stack with Array/Stack.py
+++ Stack with Array/Stack.py
@@ -1,32 +1,37 @@
+# Stack class represents a simple stack data structure
 class Stack:
-    def __init__(self,c):
-        self.stack = [None for i in range(c)]
-        self.top = -1
-        self.capacity = c
+    def __init__(self, c):
+        self.stack = [None for i in range(c)]  # List to store stack elements
+        self.top = -1  # Index of the top element in the stack
+        self.capacity = c  # Maximum capacity of the stack
 
     def isempty(self):
-        if self.top == 0:
-            return True
-        
+        # Check if the stack is empty
+        return self.top == -1
+
     def isfull(self):
-        if self.top == self.capacity:
-            return True
-        
-    def push(self,x):
-        if (self.isfull()):
-            return False
+        # Check if the stack is full
+        return self.top == self.capacity - 1
+
+    def push(self, x):
+        # Push an element onto the stack
+        if self.isfull():
+            return False  # Stack is full, cannot push
         else:
-            self.stack[self.top + 1] = x
-            self.top = self.top + 1
+            self.top += 1
+            self.stack[self.top] = x
+            return True
 
     def pop(self):
-        if(self.isempty()):
-            return False
+        # Pop an element from the stack
+        if self.isempty():
+            return False  # Stack is empty, cannot pop
         else:
             temp = self.stack[self.top]
             self.stack[self.top] = None
-            self.top = self.top - 1
+            self.top -= 1
             return temp
 
     def peek(self):
-        return self.stack[self.top] 
+        # Return the top element without removing it
+        return self.stack[self.top]
