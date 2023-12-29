59285d8d651a1c10e0873fe2222673ec784c59d9
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-29 17:22:56 +0330

    comment bucket Sort

--- Sort/Bucket Sort.py
+++ Sort/Bucket Sort.py
@@ -1,19 +1,30 @@
+# Import the selectionSort function from the Selection_Sort module
 from Selection_Sort import selectionSort
+
+# Function to perform Bucket Sort on an array
 def BucketSort(array):
+    # Find the maximum and minimum values in the array
     maxVal = max(array)
     minVal = min(array)
     
+    # Calculate the range of each bucket
     bucketRange = (maxVal - minVal) / len(array)
     
-    bucket = [[]for i in range(len(array))]
+    # Create empty buckets (lists of lists)
+    buckets = [[] for _ in range(len(array))]
     
+    # Distribute elements into buckets
     for n in array:
+        # Calculate the index of the bucket for the current element
         index = int((n - minVal) / bucketRange)
-        bucket[index].append(n)
+        # Append the element to the corresponding bucket
+        buckets[index].append(n)
         
-    for i in range(len(bucket)):
-        selectionSort(bucket[i])
+    # Sort each bucket using the selectionSort function
+    for i in range(len(buckets)):
+        selectionSort(buckets[i])
         
-    sortedArray = [n for finalBucket in bucket for n in finalBucket]
+    # Concatenate the sorted buckets into the final sorted array
+    sortedArray = [n for finalBucket in buckets for n in finalBucket]
     
     return sortedArray
