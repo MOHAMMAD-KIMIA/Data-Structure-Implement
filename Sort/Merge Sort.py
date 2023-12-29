eb3096cd3d890fca4ed18667ed37c694fc32d84d
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2023-12-29 16:47:58 +0330

    delete driver code of merge Sort

--- Sort/Merge Sort.py
+++ Sort/Merge Sort.py
@@ -26,11 +26,3 @@ def mergeSort(array):
             array[k] = right[r]
             r += 1
             k += 1
-            
-if __name__ == "__main__":
-    unsorted_array = [38, 27, 43, 3, 9, 82, 10]
-    print("Original array:", unsorted_array)
-
-    mergeSort(unsorted_array)
-
-    print("Sorted array:", unsorted_array)
